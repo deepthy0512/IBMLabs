dd09d888e3631410d383d2753208887d50c70342
deepthy0512 <60912799+deepthy0512@users.noreply.github.com>
2020-02-24 22:14:54 -0800

    Add files via upload

---
+++ list.py
@@ -0,0 +1,8 @@
+fruits = {"apple","mango","orange"}
+print(fruits)
+vegetable = ("carrot","potato")
+print(vegetable)
+alphabets = {'a':'apple','b':'banana','c':'cat'}
+print(alphabets)
+vowels = {'a','e','i','o','u'}
+print(vowels)
